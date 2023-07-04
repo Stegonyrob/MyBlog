4dec687d885ee0506f35f749a39dd32f7abb8c58
Stegonyrob <127294546+Stegonyrob@users.noreply.github.com>
2023-07-04 15:58:29 +0200

    bd

--- vamos/soy_linux.sql
+++ vamos/soy_linux.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-07-2023 a las 13:35:36
+-- Tiempo de generación: 04-07-2023 a las 15:33:10
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -31,7 +31,7 @@ CREATE TABLE `abaut` (
   `id` int(11) NOT NULL,
   `user_id` varchar(50) NOT NULL,
   `title` varchar(50) NOT NULL,
-  `coment` varchar(250) NOT NULL,
+  `coment` text NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -40,7 +40,7 @@ CREATE TABLE `abaut` (
 --
 
 INSERT INTO `abaut` (`id`, `user_id`, `title`, `coment`, `date`) VALUES
-(1, '', 'Sobre Mi', 'Que les puedo contar sobre, tal vez explicar por que el nombre del blog, en una ocasión escuche una explicación que existen por lo menos dos sistemas operativos en el cerebro de las personas , windows en la mayoria de la sociedad occidental y y linux', '0000-00-00');
+(1, '', 'Sobre Mi', 'Que les puedo contar sobre mi, tal vez explicar por que el nombre del blog, en una ocasión escuche una explicación que existen por lo menos dos sistemas operativos en el cerebro de las personas, windows en la mayoria de la sociedad occidental y linux en la mayoria de la sociedad oriental y en los nordicos, por ello algunos de los linux vivimos con muchos windows y algunos windows les toca vivir con muchos linux, y esta bien no hay problema de que sea así solo que debemos saber complementarnos para que, todos podamos convivir sin que nos dañe, respetando las pequeñas particularidades de cada uno, por esto es lo que me considero linux ya que no siempre pienso de la misma manera que los demas.', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -52,7 +52,7 @@ CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `content` text NOT NULL,
-  `image` varchar(255) DEFAULT NULL,
+  `image` varchar(5000) DEFAULT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -61,26 +61,12 @@ CREATE TABLE `posts` (
 --
 
 INSERT INTO `posts` (`id`, `title`, `content`, `image`, `createdAt`) VALUES
-(3, 'Texturas de Ropa', 'También puede ocurrir que las texturas de las ropas nos desagraden, o las etiquetas, a algunos de nosotros nos gusta que la ropa sea olgada y ligera, a otros todo lo contrario debe ser pesada y apretada, ya que nos sentimos mejor así más seguros, otros de nosotros ocupamos ambas pero según en que ambito estemos, en casa ligera y olgada, y en la calle apretada y pesada, tampoco soportamos todos los tipos de tegidos, algunos nos disgustan más que otros, a algunos nos gustan los suaves al estilo de los tegidos sinteticos o la seda, y a otros algo más aspero o de textura maás densa como la lana.', 'comida.jpg', '2023-06-28 20:26:24'),
-(4, 'Texturas', 'Otra cosa que nos caracteriza es que no todas las texturas que existen nos gustan, a algunos de nosotros nos mueve una necesidad inherente de tocar todo, y a otros tan solo por ver una superficie ya nos genera desagrado y nos cuesta tocarla, como la arena o el cesped, algunos no nos gusta ensuciarnos que algo nos toque seco o húmedo y nos deje una mancha como la de harina o pintura, nos extresa. \r\n   ', 'superficies.jpg', '2023-06-28 20:26:24'),
-(5, 'Comunicación', 'Todos nosotros nos comunicamos, puede ser que no sea como estás acostumbrado, pero siempre presume competencía. Ten esto en consideración nuestros saltos, gritos, palmadas, susurros, valanceos y también son comunicación, no nos juzgues por ello. Puede que no hablemos en lo absoluto pero nuestros gestos, posturas y actitudes son también comunicación. No hace falta que te miremos a los ojos, lo mismo te escuchamos, si me exiges que te mire a los ojos debo esforzarme en ello y mis pensamientos se enfocan en \"debo mirar a los ojos, debo mirar a los ojos\" y no logro prestarte atención a lo que dices, no es que te oculte algo.', 'comunicacion.jpg', '2023-06-28 20:29:25'),
-(6, 'Comidas', 'No todos de nosotros podemos comer de todo, ya que algunas texturas, colores, olores pueden probocar que nos sea desagradable, puede ocurrir que un sabor nos guste pero el como se siente en nuestra boca no, y por ello ante la sensación incomoda que nos genera decidimos mejor no comerlo. ', 'comida.jpg', '2023-06-28 20:30:18'),
-(7, 'hola', 'aqui probando', '', '2023-06-29 11:45:53'),
-(8, 'hola', 'aqui probando', '', '2023-06-29 11:46:07'),
-(19, 'asdkjf', 'asdfg', NULL, '2023-07-01 17:56:35'),
-(20, 'asdkjf', 'asdfg', NULL, '2023-07-01 17:56:38'),
-(21, 'asdkjf', 'asdfg', NULL, '2023-07-01 17:56:39'),
-(22, 'asdkjf', 'asdfg', NULL, '2023-07-01 17:56:39'),
-(23, 'asdkjf', 'asdfg', NULL, '2023-07-01 17:56:39'),
-(24, 'asdkjf', 'asdfg', NULL, '2023-07-01 17:56:40'),
-(25, 'asdkjf', 'asdfg', NULL, '2023-07-01 17:56:40'),
-(26, 'asdkjf', 'asdfg', NULL, '2023-07-01 17:56:40'),
-(27, 'asdkjf', 'asdfg', NULL, '2023-07-01 17:56:40'),
-(28, 'vanos', 'peuba 3021', NULL, '2023-07-01 18:11:03'),
-(29, 'vanos', 'peuba 3021', NULL, '2023-07-01 18:14:00'),
-(30, 'vanos', 'peuba 3021', NULL, '2023-07-01 18:14:05'),
-(31, 'vamos a probar las imagenes', 'askdjfhsdkjf', NULL, '2023-07-01 18:57:19'),
-(32, 'vamos a probar las imagenes', 'askdjfhsdkjf', NULL, '2023-07-01 18:57:34');
+(87, 'Comidas', 'No todos de nosotros podemos comer de todo, ya que algunas texturas, colores, olores pueden probocar que nos sea desagradable, puede ocurrir que un sabor nos guste pero el como se siente en nuestra boca no, y por ello ante la sensación incomoda que nos genera decidimos mejor no comerlo.', '1688475066472-398522654.jpg', '2023-07-04 12:51:06'),
+(88, 'Comunicación', 'Todos nosotros nos comunicamos, puede ser que no sea como estás acostumbrado, pero siempre presume competencía. Ten esto en consideración nuestros saltos, gritos, palmadas, susurros, valanceos y también son comunicación, no nos juzgues por ello. Puede que no hablemos en lo absoluto pero nuestros gestos, posturas y actitudes son también comunicación. No hace falta que te miremos a los ojos, lo mismo te escuchamos, si me exiges que te mire a los ojos debo esforzarme en ello y mis pensamientos se enfocan en \"debo mirar a los ojos, debo mirar a los ojos\" y no logro prestarte atención a lo que dices, no es que te oculte algo.', '1688475117299-831632246.jpg', '2023-07-04 12:51:57'),
+(89, 'Texturas de la Ropa', 'También puede ocurrir que las texturas de las ropas nos desagraden, o las etiquetas, a algunos de nosotros nos gusta que la ropa sea olgada y ligera, a otros todo lo contrario debe ser pesada y apretada, ya que nos sentimos mejor así más seguros, otros de nosotros ocupamos ambas pero según en que ambito estemos, en casa ligera y olgada, y en la calle apretada y pesada, tampoco soportamos todos los tipos de tegidos, algunos nos disgustan más que otros, a algunos nos gustan los suaves al estilo de los tegidos sinteticos o la seda, y a otros algo más aspero o de textura maás densa como la lana.', '1688475166319-737604982.jpg', '2023-07-04 12:52:46'),
+(90, 'Animales', 'Como a muchas personas nos gustan los animales , no a todos nos gustan lo mismo , igual que les pasa a los windows , que no todos los animales les gustan y no a todos les gustan los mismos. Pero algo que si nos destaca es que nos solemos sentir más comodos con los animales ya que ellos son más simples y no debemos estar tan atentos a no generar malas interpretaciones de nuestros comentarios o acciones.', '1688475468228-335443406.jpg', '2023-07-04 13:18:27'),
+(91, 'Texturas en general', 'Otra cosa que nos caracteriza es que no todas las texturas que existen nos gustan, a algunos de nosotros nos mueve una necesidad inherente de tocar todo, y a otros tan solo por ver una superficie ya nos genera desagrado y nos cuesta tocarla, como la arena o el cesped, algunos no nos gusta ensuciarnos que algo nos toque seco o húmedo y nos deje una mancha como la de harina o pintura, nos extresa. ', '1688475800595-248495917.jpg', '2023-07-04 13:03:20'),
+(92, 'Mucha Gente o Mucho Ruido', 'Estas dos cosas suelen ir de la mano, rar vez donde hay mucha gente , no hay mucho ruido, es casi inevitable que se genere ese cuchicheo incesante, los ruidos ambientales como cafeterías muy llenas pueden hacer que llegado un momento necesitemos tomar un descanso, de ese ambiente que nuestros oidos capten tantos sonidos a la vez que se nos vuelva inviable, segir en ese ambiente y pero eso no quiere decir que seamos antisociales o mal educados, solo que necesitamos descansar para retomar nuevamnete.', '1688477541710-680616470.jpg', '2023-07-04 13:32:21');
 
 --
 -- Índices para tablas volcadas
@@ -113,7 +99,7 @@ ALTER TABLE `abaut`
 -- AUTO_INCREMENT de la tabla `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
